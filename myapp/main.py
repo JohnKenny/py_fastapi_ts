473f1a4b6483f8aa33984f2eeac66084aab7b152
JohnKenny <squareindigojohn@yahoo.ie>
2026-04-21 14:03:56 +0100

    simple post request

--- myapp/main.py
+++ myapp/main.py
@@ -5,5 +5,9 @@ from fastapi import FastAPI
 app = FastAPI(title="My fast API", description="this is my API")
 
 @app.get("/hello-world")
-def ola():
+def hello():
     return{"Mensaje": "Ola mundo"}
+
+@app.post("/hello-world-post")
+def goodbye():
+    return{"Message":"posted to"}
