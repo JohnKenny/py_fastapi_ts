0ebe4432cbfcea22783425096f97459bd035a622
JohnKenny <squareindigojohn@yahoo.ie>
2026-05-07 21:10:22 +0100

    path param added

--- myapp/routes/hello_world.py
+++ myapp/routes/hello_world.py
@@ -2,8 +2,9 @@ from fastapi import APIRouter, Response
 
 router = APIRouter()
 
-@router.get("/hello-world")
-async def hello_world(name: str, city: str = "Londom"):
+@router.get("/hello-world/{value}")
+async def hello_world(value, name: str, city: str = "Londom"):
+    print("Test", value)
     print(f"Name: {name.title()}, City: {city.title()}")
     return {f"Hello world.. {name.title()}, {city.title()}"}
 
