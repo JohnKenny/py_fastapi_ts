bd2b27cd48b4054dc47b3e3bdd0e09d678780558
JohnKenny <squareindigojohn@yahoo.ie>
2026-05-07 17:15:18 +0100

    optional added

--- myapp/routes/hello_world.py
+++ myapp/routes/hello_world.py
@@ -3,5 +3,7 @@ from fastapi import APIRouter, Response
 router = APIRouter()
 
 @router.get("/hello-world")
-async def hello_world():
-    return {"Hello world"}
+async def hello_world(name: str, city: str = "Londom"):
+    print(f"Name: {name.title()}, City: {city.title()}")
+    return {f"Hello world.. {name.title()}, {city.title()}"}
+
